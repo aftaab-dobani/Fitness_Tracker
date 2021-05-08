eb0c1c07bbf455484d9c6fa5da95dd2d6a3c7384
Aftaab Dobani <1234dobani@gmail.com>
2021-05-08 15:32:05 -0400

    added server info

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const PORT = 3001; 
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use(express.static("public")); 
+
+//connect to MONGO DB library 
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+
+app.listen(PORT, () => {
+    console.log('App running on PORT ${PORT}');
+})
