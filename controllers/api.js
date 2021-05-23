1ad37136afbaa22cbd46c7b125060a9c623baa44
Aftaab Dobani <1234dobani@gmail.com>
2021-05-23 18:36:19 -0400

    added workout schema

--- controllers/api.js
+++ controllers/api.js
@@ -1,1 +1,15 @@
-const 
+const router = require('express').Router();
+
+router.post('/api/workouts/', (req, res) => {
+    Workout.create(req.body)
+    .then((workoutdb) => {
+        res.json(workoutdb)
+    })
+    .catch((error) => {
+        res.json(error)
+    })
+})
+
+router.put('/api/workouts/:id', (req, res) => {
+    
+})
---
+++ models/workouts.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            name: {
+                type: String,
+                trim: true,
+            },
+            type: {
+                type: String,
+                trim: true,
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            }
+        }
+    ]
+})
