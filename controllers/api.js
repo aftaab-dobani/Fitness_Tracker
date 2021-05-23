ac57db5e5a44a17c57cd9739f97567cd945269ce
Aftaab Dobani <1234dobani@gmail.com>
2021-05-23 19:09:08 -0400

    workouts get route added

--- controllers/api.js
+++ controllers/api.js
@@ -3,7 +3,7 @@ const Workout = require('../models/workouts');
 const router = require('express').Router();
 
 // POST 
-router.post("/api/workouts", (req, res) => {
+router.post('/api/workouts', (req, res) => {
     Workout.create({})
     .then(data => res.json(data))
     .catch(err => res.status(500).json(err))
@@ -12,6 +12,21 @@ router.post("/api/workouts", (req, res) => {
 // PUT 
 router.put('/api/workouts/:id', (req, res) => {
     Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}}, {new: true, runValidators: true})
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json(err))
+})
+
+// GET 
+router.get('/api/workouts', (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: '$exercises.duration'
+                }
+            }
+
+    }])
     .then(data => res.json(data))
     .catch(err => res.status(500).json(err))
 })
