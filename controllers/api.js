effa549fad4b85a2576978e79de06e45c2854539
Aftaab Dobani <1234dobani@gmail.com>
2021-05-23 18:48:50 -0400

    added put route

--- controllers/api.js
+++ controllers/api.js
@@ -9,6 +9,10 @@ router.post("/api/workouts", (req, res) => {
     .catch(err => res.status(500).json(err))
 })
 
+// PUT 
 router.put('/api/workouts/:id', (req, res) => {
-    
+    Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}}, {new: true, runValidators: true})
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json(err))
 })
+
