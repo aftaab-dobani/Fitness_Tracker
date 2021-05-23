e1d8bac0bf856a06fbea9796f90546345ced7500
Aftaab Dobani <1234dobani@gmail.com>
2021-05-23 18:46:09 -0400

    added post route

--- controllers/api.js
+++ controllers/api.js
@@ -1,13 +1,12 @@
+const Workout = require('../models/workouts');
+
 const router = require('express').Router();
 
-router.post('/api/workouts/', (req, res) => {
-    Workout.create(req.body)
-    .then((workoutdb) => {
-        res.json(workoutdb)
-    })
-    .catch((error) => {
-        res.json(error)
-    })
+// POST 
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+    .then(data => res.json(data))
+    .catch(err => res.status(500).json(err))
 })
 
 router.put('/api/workouts/:id', (req, res) => {
